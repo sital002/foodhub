de4ab84a8b687dfc2dd150bdba2cc9a181cedbe3
sitaladhikari002@gmail.com <sitaladhikari002@gmail.com>
2023-07-13 21:54:56 +0545

    update:added category section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,29 @@
 
 import CategoryCard from "@/components/category-card/category-card";
 import Wrapper from "@/components/wrapper/wrapper";
+import Image from "next/image";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import backeryImg from '../assets/bakery.png';
+import heroSectionImg from '../assets/hero-section-image.png';
+import nonVegImg from '../assets/non-veg-resturant.png'
+import veGImg from '../assets/veg-resturant.png'
 
 
 export default function Home() {
-  const CarouselData  = [
-    {
-      url:"https://merokinmel.com/storage/uploads/banner/64280e6f37fdc.png",
-    }
-  ] 
   return (
     <main>
       <Carousel autoPlay={true} className="my-2" showThumbs={false}>
-                <div>
-                    <img src="https://merokinmel.com/storage/uploads/banner/64280e6f37fdc.png" />
-                </div>
-                <div>
-                    <img src="https://merokinmel.com/storage/uploads/banner/64280e6f37fdc.png" />
-                </div>
-            </Carousel>
-            <Wrapper>
-            <h2 className="text-2xl font-bold my-2">Shop By Categories</h2>
-            <p className="text-gray-500">We've Got Something For Everyone</p>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            </Wrapper>
+        <div><Image src={heroSectionImg} width={500} height={500} alt="Food Hub" /></div>
+        <div><Image src={heroSectionImg} width={500} height={500} alt="Food Hub" /></div>
+      </Carousel>
+      <Wrapper>
+        <h2 className="text-2xl font-bold my-2">Shop By Categories</h2>
+        <p className="text-gray-500">We've Got Something For Everyone</p>
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+      </Wrapper>
     </main>
   )
 }
--- src/components/category-card/category-card.tsx
+++ src/components/category-card/category-card.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
+import vegImg from '../../assets/veg-resturant.png'
 
 const CategoryCard = () => {
   return (
     <div className='max-w-xs w-full cursor-pointer mr-4 my-2 inline-block rounded-2xl p-3 shadow-lg border-solid border-2 ease-in duration-200 hover:border-sky-500'>
         <Image
         className='w-48 mx-auto' 
-         src="https://merokinmel.com/storage/category/background/6426dccdaf285.png" alt="image" />
+         src={vegImg} alt="VEg Resturant" />
         <p className='text-center text-xl my-2 font-bold'>Veg Resturant</p>
     </div>
   )
