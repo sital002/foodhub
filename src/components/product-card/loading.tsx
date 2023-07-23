e04f2c00efd2c0071c6abb631817674ed7e9b2ac
sitaladhikari002@gmail.com <sitaladhikari002@gmail.com>
2023-07-23 13:14:47 +0545

    update: added user schema

---
+++ src/components/product-card/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingSkeleton from '@utils/LoadingSkeleton'
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <LoadingSkeleton />
+
+  }
---
+++ src/models/UserModel.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const UserSchema =new  mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Name is required"]
+    },
+    email:{
+        type:String,
+        required:[true,"Email is required"]
+    },
+    role:{
+        type:String,
+        default:"user"
+    },
+    profile:{
+        url:String
+    },
+    mobile:{
+        type:Number,
+
+    },
+    cart:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"product"
+        }
+    ],
+    orders:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"product"
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default: Date.now()
+    }
+});
+
+
+
+
+UserSchema.methods.getCartItems = function(){
+    return this.cart;
+}
+
+
+export const User = mongoose.model("user",UserSchema);
